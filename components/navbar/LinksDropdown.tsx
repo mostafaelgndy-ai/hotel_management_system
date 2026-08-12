58a4f3f61885e0cbd857ee87f509ac6e4f14e028
Mahmoud Shahin <Hiddenlockteam.KernCore@gmail.com>
2026-08-13 01:03:10 +0200

    Fix: add new changes to next auth setup

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   DropdownMenu,
@@ -6,42 +6,53 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
   DropdownMenuSeparator,
-} from '@/components/ui/dropdown-menu';
-import { LuAlignLeft, LuUser } from 'react-icons/lu';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { SignedIn, SignedOut } from '@clerk/nextjs';
-import UserProfileDropdown from './UserProfileDropdown';
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft, LuUser } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
+import UserProfileDropdown from "./UserProfileDropdown";
 
 export default function LinksDropdown() {
+  const { status } = useSession();
+
+  // في حالة التحميل يمكنك إرجاع زر بشكل محايد أو شاشة Skeleton صغيرة
+  if (status === "loading") {
+    return (
+      <Button variant="outline" className="flex gap-4 max-w-[100px]" disabled>
+        <LuAlignLeft className="w-6 h-6" />
+        <LuUser className="w-6 h-6" />
+      </Button>
+    );
+  }
+
+  // إذا كان المستخدم مسجلاً لدخوله
+  if (status === "authenticated") {
+    return <UserProfileDropdown />;
+  }
+
+  // إذا كان غير مسجل للدخول (Unauthenticated)
   return (
-    <>
-      <SignedOut>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="flex gap-4 max-w-[100px]">
-              <LuAlignLeft className="w-6 h-6" />
-              <LuUser className="w-6 h-6" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-40" align="start" sideOffset={10}>
-            <DropdownMenuItem asChild>
-              <Link href="/sign-in" className="w-full cursor-pointer">
-                Login
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link href="/sign-up" className="w-full cursor-pointer">
-                Register
-              </Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SignedOut>
-      <SignedIn>
-        <UserProfileDropdown />
-      </SignedIn>
-    </>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="w-6 h-6" />
+          <LuUser className="w-6 h-6" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="start" sideOffset={10}>
+        <DropdownMenuItem asChild>
+          <Link href="/api/auth/signin" className="w-full cursor-pointer">
+            Login
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/register" className="w-full cursor-pointer">
+            Register
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
