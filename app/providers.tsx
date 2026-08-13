109276ddddd85c07ae2c664cdedd80bdf235c446
Mahmoud Shahin <Hiddenlockteam.KernCore@gmail.com>
2026-08-13 05:42:26 +0200

    added session provider and remove clerk from pachage json

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,23 @@
-'use client';
-import { ThemeProvider } from './theme-provider';
-import { Toaster } from '@/components/ui/toaster';
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <SessionProvider>
       <Toaster />
       <ThemeProvider
-        attribute='class'
-        defaultTheme='system'
+        attribute="class"
+        defaultTheme="system"
         enableSystem
         disableTransitionOnChange
       >
         {children}
       </ThemeProvider>
-    </>
+    </SessionProvider>
   );
 }
+
 export default Providers;
